9dfd96ccf0516a541bdb26d3f03fad0179de335f
Insxmniaa <jeanne.melendez@icloud.com>
2024-03-21 16:50:55 -0400

    betty main.h

--- main.h
+++ main.h
@@ -4,6 +4,12 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct coversion - allows us to declare variables.
+ * @string: a pointer to a character array.
+ * @f: takes a variable argument list and returns an integer.
+ */
+
 typedef struct conversion
 {
 char *string;
@@ -15,4 +21,5 @@ int print_char(va_list args);
 int _putchar(char c);
 int print_string(va_list args);
 int print_perc(va_list args);
+
 #endif
